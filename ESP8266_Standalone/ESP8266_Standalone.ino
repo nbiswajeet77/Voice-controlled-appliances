735a4a216c014ca91445064a2c4ac62970f03e44
nbiswajeet77 <47244652+nbiswajeet77@users.noreply.github.com>
2019-10-13 16:11:58 +0530

    Add files via upload
    Arduino code which was fed into the microcontroller. The remaining settings were done by the third party applet services(IFTTT).

---
+++ ESP8266_Standalone/ESP8266_Standalone.ino
@@ -0,0 +1,23 @@
+
+#define BLYNK_PRINT Serial
+#include <ESP8266WiFi.h>
+#include <BlynkSimpleEsp8266.h>
+
+char auth[] = "9bb13419171b47b189cc58fbc895dc46";
+
+char ssid[] = "Lenovo K6 POWER";
+char pass[] = "12345678";
+
+void setup()
+{
+  // Debug console
+  Serial.begin(9600);
+
+  Blynk.begin(auth, ssid, pass);
+}
+
+void loop()
+{
+  Blynk.run();
+}
+
